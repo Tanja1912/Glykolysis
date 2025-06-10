f5ec8bf038a608526ec3fc8b6f178d94b80939c5
Tanja1912 <aufhammer.tanja@gmail.com>
2025-06-10 10:14:38 +0200

    Update Glykolyse_1.py

--- Glykolyse_1.py
+++ Glykolyse_1.py
@@ -143,7 +143,7 @@ class GlycolysisPathway:
             "Pyruvat": [],
         }
 
-        for reaction in range(steps):                                                                            #durchlaufen der Schleife steps-mal
+        for _ in range(steps):                                                                            #durchlaufen der Schleife steps-mal
             for reaction in self.reactions:                                                               #aufrufen der Methode step(dt) für jede Reaktion
                 reaction.step(dt)                                                                        
                 
